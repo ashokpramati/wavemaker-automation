4214bfa41664edc39b7b36ad3ec1c731213b34ab
nellorekrishnakumar <krishnakumar.nellore@imaginea.com>
2013-09-17 09:48:38 +0530

    Updated Test case with comments and added few assertion. in test case.

--- src/test/java/com/pramati/cloudjee/test/rest/MaxWarUploadTest.java
+++ src/test/java/com/pramati/cloudjee/test/rest/MaxWarUploadTest.java
@@ -3,6 +3,7 @@ package com.pramati.cloudjee.test.rest;
 import org.apache.log4j.Logger;
 import org.codehaus.jackson.JsonNode;
 import org.json.simple.parser.ParseException;
+import org.testng.Assert;
 import org.testng.annotations.Test;
 
 import com.pramati.cloudjee.utils.BaseTest;
@@ -18,13 +19,15 @@ public class MaxWarUploadTest extends BaseTest {
 
 	protected static Logger log = Logger.getLogger(RestApiTest.class);
 
-	@Test	
+	private static final String MAX_DEPLOY = "maxdeploy";
+	private static final String NODE_CAUSE = "cause";
+
+	@Test(description="Maximum application deployment test case, Only 5 Application war file need to be uploaded in cloud")	
 	public void verifyMaxDeployApplicationAPI() throws ParseException {
-		log.info("Started deploying of APP.");
-		
-		String JsonResponse = RestApiCall.getJSONResponse("maxdeploy");
-		log.info("Completed deployment of APP.");
+		log.info("Started running maximum APP deployment.");		
+		String JsonResponse = RestApiCall.getJSONResponse(MAX_DEPLOY);
+		log.info("Completed max app deployment.");
 		JsonNode node = getJsonNode(JsonResponse);
-		System.out.println(node.getValueAsText());
+		Assert.assertEquals(node.findValues(NODE_CAUSE).get(0).getTextValue(),"Maximun of 5 already deployed.","More than 5 application deployment limit is crossed");
 	}
 }
--- src/test/java/com/pramati/cloudjee/test/rest/RestApiTest.java
+++ src/test/java/com/pramati/cloudjee/test/rest/RestApiTest.java
@@ -26,9 +26,24 @@ import com.pramati.cloudjee.utils.RestApiCall;
  */
 public class RestApiTest extends BaseTest {
 
+	private static final String NODE_TIME_TAKEN = "time-taken";
+	private static final String DEPLOY = "deploy";	
+	private static final String UNDEPLOY = "undeploy";
+	private static final String LIST ="list";
+	
+	
+	private static final String NODE_APPSTATE ="appState";	
+	private static final String NODE_APPFILENAME ="appFileName";
+	private static final String NODE_TYPE = "type";
+	private static final String NODE_SERVED_BY =  "served-by";
+	private static final String NODE_INSTANCEGROUPNAME= "instanceGroupName";
+	private static final String NODE_URL ="url";
+	private static final String NODE_SSLENABLED="sslEnabled";
+	
 	protected static Logger log = Logger.getLogger(RestApiTest.class);
+	
 
-	@Test
+	@Test(description="Verify whether application is deployed successfully in cloud")
 	@Parameters({ "type", "appFileName", "appState", "instanceGroupName",
 			"name", "sslEnabled", "url" })
 	public void verifyDeployApplicationAPI(String type, String appFileName,
@@ -36,105 +51,108 @@ public class RestApiTest extends BaseTest {
 			String sslEnabled, String url) throws ParseException {
 		log.info("Started deploying of APP.");
 		JsonNode bodyNode = null;
-		String JsonResponse = RestApiCall.getJSONResponse("deploy");
+		String JsonResponse = RestApiCall.getJSONResponse(DEPLOY);
 		log.info("Completed deployment of APP.");
 		JsonNode node = getJsonNode(JsonResponse);
-		bodyNode = node.findValue("type");
+		bodyNode = node.findValue(NODE_TYPE);
 		Assert.assertEquals(bodyNode.getTextValue(), "application",
 				"type doesn't match");
-		bodyNode = node.findValue("appFileName");
+		bodyNode = node.findValue(NODE_APPFILENAME);
 		Assert.assertEquals(bodyNode.getTextValue(), ConfigProperties.APP_NAME
 				+ ".war", "APP Name doesn't match");
 		bodyNode = node.findValue("appState");
 		Assert.assertEquals(bodyNode.getTextValue(), "STARTED",
 				"type doesn't match");
-		bodyNode = node.findValue("instanceGroupName");
+		bodyNode = node.findValue(NODE_INSTANCEGROUPNAME);
 		Assert.assertEquals(bodyNode.getTextValue(), "defaultgroup",
 				"Default group doesn't match");
 		bodyNode = node.findValue("name");
 		Assert.assertEquals(bodyNode.getTextValue(), ConfigProperties.APP_NAME,
 				"APP Name doesn't match");
-		bodyNode = node.findValue("sslEnabled");
+		bodyNode = node.findValue(NODE_SSLENABLED);
 		Assert.assertFalse(bodyNode.getBooleanValue(), "SSL is not enabled");
-		bodyNode = node.findValue("url");
+		bodyNode = node.findValue(NODE_URL);
 		Assert.assertEquals(bodyNode.getTextValue(),
 				"http://ashok.apps.mywavemaker.com/sample/",
 				"type doesn't match");
-		Assert.assertTrue(node.findValue("time-taken").getIntValue() < 10,
+		Assert.assertTrue(node.findValue(NODE_TIME_TAKEN).getIntValue() < 10,
 				"Time taken is more than 10");
 	}
 
-	@Test(dependsOnMethods = "verifyDeployApplicationAPI")
+	@Test(dependsOnMethods = "verifyDeployApplicationAPI",description="Verify whether application is stopped succesfully")
 	public void verifyStopApplicationAPI() throws ParseException {
 		log.info("Beginning to stop APP.");
 		String JsonResponse = RestApiCall.getJSONResponse("stop");
 		log.info("Stopped APP.");
 		JsonNode node = getJsonNode(JsonResponse);
-		JsonNode bodyNode = node.findValue("served-by");
+		JsonNode bodyNode = node.findValue(NODE_SERVED_BY);
 		Assert.assertEquals(bodyNode.getTextValue(), "www.cloudjee.com");
-		Assert.assertTrue(node.findValue("time-taken").getIntValue() < 10,
+		Assert.assertTrue(node.findValue(NODE_TIME_TAKEN).getIntValue() < 10,
 				"Time taken is more than 10");
 	}
 
-	@Test(dependsOnMethods = "verifyStopApplicationAPI")
+	@Test(dependsOnMethods = "verifyStopApplicationAPI" , description="Verify whether Application is started succesfully")
 	public void verifyStartApplicationAPI() throws ParseException {
 		log.info("Starting APP.");
 		String JsonResponse = RestApiCall.getJSONResponse("start");
 		log.info("Started APP.");
 		JsonNode node = getJsonNode(JsonResponse);
-		JsonNode bodyNode = node.findValue("served-by");
+		JsonNode bodyNode = node.findValue(NODE_SERVED_BY);
 		Assert.assertEquals(bodyNode.getTextValue(), "www.cloudjee.com");
-		Assert.assertTrue(node.findValue("time-taken").getIntValue() < 10,
+		Assert.assertTrue(node.findValue(NODE_TIME_TAKEN).getIntValue() < 10,
 				"Time taken is more than 10");
 	}
-
-	@Test(dependsOnMethods = "verifyStartApplicationAPI")
+	
+	
+	@Test(dependsOnMethods = "verifyStartApplicationAPI", description="Verify list of application avalibale in cloud")
 	public void verifyListApplicationsAPI() throws ParseException {
 		log.info("Listing APP.");
 		List<JsonNode> bodyNode = null;
-		String JsonResponse = RestApiCall.getJSONResponse("list");
+		String JsonResponse = RestApiCall.getJSONResponse(LIST);
 		log.info("APP's are listed.");
 		JsonNode node = getJsonNode(JsonResponse);
-		bodyNode = node.findValues("type");
+		bodyNode = node.findValues(NODE_TYPE);
 		Assert.assertTrue(bodyNode.toString().contains("application"),
 				"Type doesn't match");
-		bodyNode = node.findValues("appFileName");
+		bodyNode = node.findValues(NODE_APPFILENAME);
 		Assert.assertTrue(
 				bodyNode.toString()
 						.contains(ConfigProperties.APP_NAME + ".war"),
 				"APP Name doesn't match");
-		bodyNode = node.findValues("appState");
+		bodyNode = node.findValues(NODE_APPSTATE);
 		Assert.assertTrue(bodyNode.toString().contains("STARTED"),
 				"type doesn't match");
-		bodyNode = node.findValues("instanceGroupName");
+		bodyNode = node.findValues(NODE_INSTANCEGROUPNAME);
 		Assert.assertTrue(bodyNode.toString().contains("defaultgroup"),
 				"Default group doesn't match");
 		bodyNode = node.findValues("name");
 		Assert.assertTrue(
 				bodyNode.toString().contains(ConfigProperties.APP_NAME),
 				"APP Name doesn't match");
-		bodyNode = node.findValues("sslEnabled");
+		bodyNode = node.findValues(NODE_SSLENABLED);
 		System.out.println(bodyNode);
 		Assert.assertFalse(bodyNode.contains(false), "SSL is not enabled");
-		bodyNode = node.findValues("url");
+		bodyNode = node.findValues(NODE_URL);
 		Assert.assertTrue(
 				bodyNode.toString().contains(
 						"http://ashok.apps.mywavemaker.com/sample/"),
 				"type doesn't match");
-		Assert.assertTrue(node.findValue("time-taken").getIntValue() < 10,
+		Assert.assertTrue(node.findValue(NODE_TIME_TAKEN).getIntValue() < 10,
 				"Time taken is more than 10");
 
 	}
+	
+	
 
-	@Test(dependsOnMethods = "verifyListApplicationsAPI")
+	@Test(dependsOnMethods = "verifyListApplicationsAPI",description="Verify whether application is undeployed succesfully from cloud")
 	public void verifyUndeployApplicationAPI() throws ParseException {
 		log.info("Undeploying APP.");
-		String JsonResponse = RestApiCall.getJSONResponse("undeploy");
+		String JsonResponse = RestApiCall.getJSONResponse(UNDEPLOY);
 		log.info("Completed undeployment of APP.");
 		JsonNode node = getJsonNode(JsonResponse);
-		JsonNode bodyNode = node.findValue("served-by");
+		JsonNode bodyNode = node.findValue(NODE_SERVED_BY);
 		Assert.assertEquals(bodyNode.getTextValue(), "www.cloudjee.com");
-		Assert.assertTrue(node.findValue("time-taken").getIntValue() < 10,
+		Assert.assertTrue(node.findValue(NODE_TIME_TAKEN).getIntValue() < 10,
 				"Time taken is more than 10");
 
 	}
